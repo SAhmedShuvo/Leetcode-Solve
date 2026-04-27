d54daecad41f7cfcfd940c41391e6c1da9af4442
sabbir <sabbir.cou15@gmail.com>
2026-04-27 12:20:04 +0600

    first commit

---
+++ 88. Merge Sorted Array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void merge(vector<int>& a, int m, vector<int>& b, int n) {
+        int i=m-1, j=n-1, k= n+m-1;
+
+        while(i>=0 && j>=0)
+        {
+            if(a[i]>b[j])
+            {
+                a[k]=a[i];
+                i--;
+                k--;
+            }
+            else 
+            {
+                a[k]=b[j];
+                j--;
+                k--;
+            }
+
+        }
+        while(j>=0)
+        {
+            a[k]= b[j];
+            k--; j--;
+        }
+
+    }
+};
